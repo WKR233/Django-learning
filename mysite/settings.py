e16f54e5e9827ee0fce7f64abbd9643a55bbd22e
Wang Keran <29503041@qq.com>
2023-07-13 19:04:27 +0800

    modified the settings.py and polished README to make it more specific

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
+    "polls.apps.PollsConfig", # install the app we wrote
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
