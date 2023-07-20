43fe17de3c79ffa419662482abfa2e26ab974f83
Wang Keran <29503041@qq.com>
2023-07-20 11:29:29 +0800

    modified the index function

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
 # Create your views here.
 
-def index(request,year):
-    text="Hello, world. You're at the polls index at "
-    text+=str(year)
-    return HttpResponse(text)
+def index(request):
+    latest_question_list=Question.objects.order_by("-pub_date")[:5]
+    # order the 5 latest questions
+    output=", ".join([q.question_text for q in latest_question_list])
+    # join is used to link these strings, [...] represents for a list
+    return HttpResponse(output)
 
 def detail(request,question_id):
     return HttpResponse("You're looking at question %s." % question_id)
