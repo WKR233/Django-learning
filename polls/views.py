6bc242ff225c55981673bf399de19f8cd17ba3b6
Wang Keran <29503041@qq.com>
2023-07-20 11:00:47 +0800

    added several func in views.py

--- polls/views.py
+++ polls/views.py
@@ -7,3 +7,13 @@ def index(request,year):
     text="Hello, world. You're at the polls index at "
     text+=str(year)
     return HttpResponse(text)
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request,question_id):
+    response="You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
