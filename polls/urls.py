31befe1617912651fe535b6438640064a0b0858d
Wang Keran <29503041@qq.com>
2023-07-20 11:09:36 +0800

    linked the func(detail  results vote) with urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path("<int:year>/",views.index,name="index"),
-]
+    # e.g. /polls/2005/
+    path("<int:question_id>/",views.detail,name="detail"),
+    # e.g. /polls/5/
+    path("<int:question_id>/results/",views.results,name="results"),
+    # e.g. /polls/5/results/
+    path("<int:question_id>/vote/",views.vote,name="vote"),
+    # e.g. /polls/5/vote/
+]# external urls: /polls/
