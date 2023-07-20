69ce7acc346a4fbff70598c047e39648754befc7
Wang Keran <29503041@qq.com>
2023-07-20 19:38:20 +0800

    load template to views.py

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
-# Create your views here.
 
 def index(request):
     latest_question_list=Question.objects.order_by("-pub_date")[:5]
     # order the 5 latest questions
-    output=", ".join([q.question_text for q in latest_question_list])
-    # join is used to link these strings, [...] represents for a list
-    return HttpResponse(output)
+    template=loader.get_template("polls/index.html")
+    context={
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
+    # load polls/index.html and pass it with context which is a dict
 
 def detail(request,question_id):
     return HttpResponse("You're looking at question %s." % question_id)
